0349fa597de1267b1256afbe4c688f4ead924df5
SalvaSC <sanchezcampossalvador@gmail.com>
2020-11-21 10:23:55 +0100

    Cosis que ya he hecho y no me deja hacer el pull origin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 const  App = () => {
   return (
-    <div>
-			<h1 style={{textAlign: "center"}}>
-				Nuclio FullStack Development Hackaton 2
-			</h1>
-    </div>
+    <Router>
+        <NavBar setContainerClass={setContainerClass} />
+      <div className={containerClass}>
+        <div className="main">
+          <Switch>
+            <Route exact path={HOME} component={Home} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
